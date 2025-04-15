4b4f2265dd3aab21d451315dbf9d6944f03cb1b5
LiliyaShammasova <leelia@mail.ru>
2025-04-15 12:02:25 +0300

    change RandomNumber

--- MyProjectsPython/RandomNumber/RandomNumber.py
+++ MyProjectsPython/RandomNumber/RandomNumber.py
@@ -20,7 +20,7 @@ while True:
             continue
         elif n == num:
             print('Вы угадали, поздравляем!\n Хотите сыграть еще? (1 - да, все остальное - нет)')
-            if int(input()) == 1:
+            if input() == '1':
                 num = random.randint(1, 100)
                 print("Введите целое число от 1 до 100.")
                 continue
